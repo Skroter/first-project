3bb9acbaaa9ec8308adf9d0b076e0e3bcb326a92
DiGro <DiGro@yandex.ru>
2022-02-08 15:40:43 +0500

    Объявлен класс в Converter. В Praktikum создан конструктор

--- Praktikum.java
+++ Praktikum.java
@@ -1,5 +1,11 @@
 class OCHKOshNIKI
+        String bratUshki;
+        
+        public OCHKOshNIKI (String names){
+                String bratUshki = names;                
+        }
 
 public static void main(String[]args){
-        
+        OCHKOshNIKI people = new OCHKOshNIKI;
+        System.out.println (people);
         }
